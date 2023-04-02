c66b1f1351b94279f26fd2044a70f3dd0127ddc0
Dylan <grey@thisthatapp.com>
2023-04-02 20:07:43 +0100

    Add components.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StaffListComponent } from './components/staff-list/staff-list.component';
+import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
+import { RecipeItemComponent } from './components/recipe-item/recipe-item.component';
+import { IngredientsListComponent } from './components/ingredients-list/ingredients-list.component';
+import { IngredientItemComponent } from './components/ingredient-item/ingredient-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaffListComponent,
+    RecipesListComponent,
+    RecipeItemComponent,
+    IngredientsListComponent,
+    IngredientItemComponent,
   ],
   imports: [
     BrowserModule,
